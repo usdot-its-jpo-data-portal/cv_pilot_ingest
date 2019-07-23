6c3d1cd2117efd8177270cc70c67bfba5284ae0e
587352 <lien_julia@bah.com>
2019-07-23 18:13:25 -0400

    Modify so that the function can be used for overwrite and non-overwrite socrata ingestion

--- lambda__lake_to_socrata.py
+++ lambda__lake_to_socrata.py
@@ -5,6 +5,7 @@ Flatten records from ITS Sandbox S3 bucket and upsert to Socrata.
 
 from __future__ import print_function
 
+from datetime import datetime, timedelta
 import logging
 import os
 import traceback
@@ -19,8 +20,15 @@ logger.setLevel(logging.INFO)  # necessary to make sure aws is logging
 SOCRATA_USERNAME = os.environ.get('SOCRATA_USERNAME')
 SOCRATA_PASSWORD = os.environ.get('SOCRATA_PASSWORD')
 SOCRATA_API_KEY = os.environ.get('SOCRATA_API_KEY')
-SOCRATA_DOMAIN = os.environ.get('SOCRATA_DOMAIN') or 'data.transportation.gov'
+SOCRATA_DOMAIN = os.environ.get('SOCRATA_DOMAIN', 'data.transportation.gov')
 SOCRATA_DATASET_ID = os.environ.get('SOCRATA_DATASET_ID')
+
+# the environment variables below are only required for lambdas that are
+# triggered by CloudWatch Events, not by S3 object uploads.
+S3_SOURCE_BUCKET = os.environ.get('S3_SOURCE_BUCKET', '')
+S3_SOURCE_PREFIX = os.environ.get('S3_SOURCE_PREFIX', '')
+NUM_HOURS_BACKTRACK = int(os.environ.get('NUM_HOURS_BACKTRACK', 48))
+
 
 socrata_params = dict(
 username = SOCRATA_USERNAME,
@@ -29,8 +37,10 @@ app_token = SOCRATA_API_KEY,
 domain = SOCRATA_DOMAIN
 )
 
-
 def load_flattener(key):
+    '''
+    Load appropriate data flattener based on pilot site and message type
+    '''
     pilot, message_type = key.split('/')[:2]
     try:
         mod = __import__('flattener_{}'.format(pilot))
@@ -43,20 +53,40 @@ def load_flattener(key):
 
 
 def lambda_handler(event, context):
-    """AWS Lambda handler. """
+    '''
+    AWS Lambda handler.
 
-    so_ingestor = SocrataDataset(dataset_id=SOCRATA_DATASET_ID,
-    socrata_client=None, socrata_params=socrata_params)
 
-    mover = CvPilotFileMover(target_bucket=TARGET_BUCKET)
+    '''
+    mover = CvPilotFileMover()
+    so_ingestor = SocrataDataset(
+        dataset_id=SOCRATA_DATASET_ID,
+        socrata_params=socrata_params,
+        float_fields=['randomNum', 'metadata_generatedAt_timeOfDay'])
 
-    for bucket, key in mover.get_fps_from_event(event):
+    if event.get('source') == 'aws.events':
+        overwrite = True
+        workingId = so_ingestor.create_new_draft()
+
+        source_ymdh = datetime.today() - timedelta(hours=NUM_HOURS_BACKTRACK)
+        y,m,d,h = source_ymdh.strftime('%Y-%m-%d-%H').split('-')
+        formatted_source_prefix = S3_SOURCE_PREFIX.format(y,m,d,h)
+        bucket_key_tuples = mover.get_fps_from_prefix(bucket=S3_SOURCE_BUCKET, prefix=formatted_source_prefix)
+        logger.log('Lambda triggered by scheduled event. Retrieved {} file paths from s3://{}/{}'.format(len(bucket_key_tuples), S3_SOURCE_BUCKET, formatted_source_prefix))
+    else:
+        # s3 triggered
+        overwrite = False
+        workingId = SOCRATA_DATASET_ID
+        bucket_key_tuples = mover.get_fps_from_event(event)
+        logger.log('Lambda triggered by uploaded s3 object. Retrieved {} file paths from event'.format(len(bucket_key_tuples)))
+
+    for bucket, key in bucket_key_tuples:
         flattener = load_flattener(key)
 
         recs = []
         err_recs = []
-        stream = prodMover.get_data_stream(bucket, key)
-        for r in prodMover.newline_json_rec_generator(stream):
+        stream = mover.get_data_stream(bucket, key)
+        for r in mover.newline_json_rec_generator(stream):
             try:
                 recs += flattener.process_and_split(r)
             except:
@@ -64,6 +94,11 @@ def lambda_handler(event, context):
                 logger.error(traceback.format_exc())
                 err_recs.append(r)
 
-        response = clean_and_upsert(recs)
+        response = so_ingestor.clean_and_upsert(recs, workingId)
+        logger.log(response)
+
+    # publish draft if this is an overwrite
+    if overwrite is True:
+        so_ingestor.publish_draft(workingId)
 
     logger.info('Processed events')

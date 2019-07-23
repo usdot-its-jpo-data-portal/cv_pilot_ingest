d31db052032b401b1135063a17f9404fc647b3d9
587352 <lien_julia@bah.com>
2019-07-23 18:12:28 -0400

    Add methods for overwriting datasets

--- socrata_util.py
+++ socrata_util.py
@@ -7,35 +7,32 @@ import copy
 import itertools
 import json
 import os
+import requests
 from sodapy import Socrata
 
 
 class SocrataDataset(object):
-    def __init__(self, dataset_id, socrata_client=None, socrata_params={}):
+    def __init__(self, dataset_id, socrata_client=None, socrata_params={}, float_fields=[]):
         self.dataset_id = dataset_id
-        self.socrata_client = socrata_client
+        self.client = socrata_client
         self.col_dtype_dict = self.get_col_dtype_dict()
         if not socrata_client and socrata_params:
-            self.socrata_client = Socrata(**socrata_params)
+            self.client = Socrata(**socrata_params)
 
     def get_col_dtype_dict(self):
         '''
         Retrieve data dictionary of a Socrata data set in the form of a dictionary,
         with the key being the column name and the value being the column data type
 
-    	Parameters:
-    		socrata_client: connected sodapy Socrata client
-            dataset_id: ID of Socrata data set
-
     	Returns:
     		data dictionary of a Socrata data set in the form of a dictionary,
             with the key being the column name and the value being the column data type
         '''
-        dataset_col_meta = self.socrata_client.get_metadata(self.dataset_id)['columns']
+        dataset_col_meta = self.client.get_metadata(self.dataset_id)['columns']
         col_dtype_dict = {col['name']: col['dataTypeName'] for col in dataset_col_meta}
         return col_dtype_dict
 
-    def mod_dtype(self, rec, col_dtype_dict=None, float_fields=[]):
+    def mod_dtype(self, rec, col_dtype_dict=None, float_fields=None):
         '''
         Make sure the data type of each field in the data record matches the data type
         of the field in the Socrata data set.
@@ -51,6 +48,7 @@ class SocrataDataset(object):
             modified to align with the data type of the corresponding Socrata data set
         '''
         col_dtype_dict = col_dtype_dict or self.col_dtype_dict
+        float_fields = float_fields or self.float_fields
 
         identity = lambda x: x
         dtype_func = {'number': float, 'text': str, 'checkbox': bool}
@@ -63,7 +61,22 @@ class SocrataDataset(object):
                     out[k] = dtype_func.get(col_dtype_dict.get(k, 'nonexistentKey'), identity)(v)
         return out
 
-    def clean_and_upsert(self, recs):
+    def create_new_draft(self):
+        draftDataset = requests.post('https://{}/api/views/{}/publication.json'.format(self.client.domain, self.dataset_id),
+                                  auth=(self.socrata_params.username, self.socrata_params.password),
+                                  params={'method': 'copySchema'})
+        logger.info(draftDataset.json())
+        draftId = draftDataset.json()['id']
+        return draftId
+
+    def publish_draft(self, draftId):
+        publishResponse = requests.post('https://{}/api/views/{}/publication.json'.format(self.client.domain, draftId),
+                                        auth=(self.socrata_params.username, self.socrata_params.password))
+        logger.info(publishResponse.json())
+        return publishResponse
+
+    def clean_and_upsert(self, recs, dataset_id=None):
+        dataset_id = dataset_id or self.dataset_id
         out_recs = [self.mod_dtype(r) for r in recs]
-        uploadResponse = self.socrata_client.upsert(self.dataset_id, out_recs)
+        uploadResponse = self.client.upsert(dataset_id, out_recs)
         return uploadResponse

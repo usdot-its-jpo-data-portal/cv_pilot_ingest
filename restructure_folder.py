aac686bf8e5377c902974cd18824a9ff9f067ad8
587352 <lien_julia@bah.com>
2019-08-23 11:54:53 -0400

    Reorganize into class obj and add option to save keys to file and read keys from file

--- restructure_folder.py
+++ restructure_folder.py
@@ -1,3 +1,6 @@
+"""
+
+"""
 from argparse import ArgumentParser
 import boto3
 import traceback
@@ -16,51 +19,84 @@ s3_credentials = {
 }
 # s3_credentials = {}
 
-def restructure_folder(bucket, folder='wydot/BSM', startKey=None, source_bucket_prefix="usdot-its-datahub-"):
+class FolderRestructurer(object):
 
-    # set up
-    s3botoclient = boto3.client('s3', **s3_credentials)
-    mover = CvPilotFileMover(target_bucket=bucket,
-                             source_bucket_prefix="",
-                             source_key_prefix="",
-                             validation_queue_name=None,
-                             log=False,
-                             s3_client=s3botoclient)
+    def __init__(self, bucket, folder='wydot/BSM', startKey='', source_bucket_prefix="usdot-its-datahub-", outfp=None, infp=None):
+        # set up
+        s3botoclient = boto3.client('s3', **s3_credentials)
+        self.mover = CvPilotFileMover(target_bucket=bucket,
+                                 source_bucket_prefix="",
+                                 source_key_prefix="",
+                                 validation_queue_name=None,
+                                 log=False,
+                                 s3_client=s3botoclient)
+        self.bucket = bucket
+        self.folder = folder
+        self.startKey = startKey
+        self.infp = infp
+        self.outfp = outfp
 
-    # get list of files to process
-    keys = mover.get_fps_from_prefix(bucket, folder)
-    print('{} keys retrieved from s3://{}/{}'.format(len(keys), bucket, folder))
-    if startKey:
-        keysFiltered = [i for i in keys if i[1] >= startKey]
-        print('Processing {} keys after start key of {}'.format(len(keysFiltered), startKey))
-    else:
-        keysFiltered = keys
+    def filter_by_startKey(self, keys):
+        if self.startKey:
+            keys = [i for i in keys if i[1] >= self.startKey]
+            print('Processing {} keys after start key of {}'.format(len(keys), self.startKey))
+        return keys
 
-    if len(keysFiltered) == 0:
-        print('All set.')
+    def get_keys_from_s3(self):
+        keys = self.mover.get_fps_from_prefix(self.bucket, self.folder)
+        print('{} keys retrieved from s3://{}/{}'.format(len(keys), self.bucket, self.folder))
+        keysFiltered = self.filter_by_startKey(keys)
 
-    # move files
-    t0 = time.time()
-    count = 0
-    for idx, tup in enumerate(keysFiltered):
-        try:
-            sb,sk = tup
-            mover.move_file(sb, sk)
-            count += 1
-        except:
-            print(traceback.format_exc())
-            print('Process stopped at idx: {}'.format(idx))
-            print('======================================')
-            print('To continue running this script on the rest of the folder, specifiy the followng --startKey for your next run:\n{}'.format(sk))
-            print('======================================')
-            break
+        if len(keysFiltered) == 0:
+            print('All set.')
 
-    t1 = time.time()
-    print('{} fps re-orged in {} hr'.format(count, (t1-t0)/3600))
+        elif self.outfp:
+            with open(self.outfp, 'w') as outfile:
+                for k in keysFiltered:
+                    outfile.write('{},{}'.format(k[0], k[1]))
+                    outfile.write('\n')
+            print('{} filepaths from s3://{}/{} written to {}'.format(len(keysFiltered), self.bucket, self.folder, self.outfp))
+        return keysFiltered
+
+    def get_keys_from_fp(self, fp):
+        with open(fp, 'r') as infile:
+            data = infile.read()
+        data = data.splitlines()
+        keys = [i.strip('\n').split(',') for i in data]
+        print('{} keys retrieved from {}'.format(len(keys), fp))
+        keysFiltered = self.filter_by_startKey(keys)
+        return keysFiltered
+
+    def run(self):
+        # get fp
+        if self.infp:
+            keysFiltered = self.get_keys_from_fp(self.infp)
+        else:
+            keysFiltered = self.get_keys_from_s3()
+
+        # move files
+        t0 = time.time()
+        count = 0
+        for idx, tup in enumerate(keysFiltered):
+            try:
+                sb,sk = tup
+                self.mover.move_file(sb, sk)
+                count += 1
+            except:
+                print(traceback.format_exc())
+                print('Process stopped at idx: {}'.format(idx))
+                print('======================================')
+                print('To continue running this script on the rest of the folder, specifiy the followng --startKey for your next run:\n{}\n'.format(sk))
+                print('======================================')
+                break
+
+        t1 = time.time()
+        print('{} fps re-orged in {} hr'.format(count, (t1-t0)/3600))
 
 
 if __name__ == '__main__':
     """
+    Sample Usage
     python -u restructure_folder.py --bucket usdot-its-cvpilot-public-data --bucket_prefix usdot-its-datahub- --folder wydot/BSM/2019/
     """
 
@@ -69,6 +105,13 @@ if __name__ == '__main__':
     parser.add_argument('--bucket_prefix', default="test-usdot-its-datahub-", help="Prefix of the s3 bucket name (e.g. 'usdot-its-datahub-' or 'test-usdot-its-datahub')")
     parser.add_argument('--folder', default=None, help="S3 folder you'd like to reorganize. All files with this prefix will be reorganized as needed, in ascending order of the key.")
     parser.add_argument('--startKey', default=None, help="Start Key - provide this only if you'd like to organize only keys after the specified key.")
-
+    parser.add_argument('--infp', default=None, help="Supply fp if you'd like to read keys to process from the file path.")
+    parser.add_argument('--outfp', default=None, help="Supply fp if you'd like to write the keys to process to a file.")
     args = parser.parse_args()
-    restructure_folder(args.bucket, folder=args.folder, startKey=args.startKey, source_bucket_prefix=args.bucket_prefix)
+    
+    folderRestructurer = FolderRestructurer(args.bucket, folder=args.folder, startKey=args.startKey, source_bucket_prefix=args.bucket_prefix, infp=args.infp, outfp=args.outfp)
+    folderRestructurer.run()
+
+
+
+
